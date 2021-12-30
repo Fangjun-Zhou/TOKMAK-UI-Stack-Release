6711bae3e4c190f18b10338a36d55d4eba9b0523
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-12-29 19:50:35 -0600

    Added: IUIPanelElement and IUIPanelStack

---
+++ Runtime/IUIPanelElement.cs
@@ -0,0 +1,53 @@
+﻿namespace FinTOKMAK.UIStackSystem.Runtime
+{
+    /// <summary>
+    /// The interface that defines the basic lifecycle of a UIPanelElement.
+    /// </summary>
+    public interface IUIPanelElement
+    {
+        /// <summary>
+        /// The callback function called when the panel change from Inactive state to Active state.
+        /// </summary>
+        void OnInactive2Active();
+
+        /// <summary>
+        /// The callback function called when the panel change from Inactive state to Background state.
+        /// </summary>
+        void OnInactive2Background();
+
+        /// <summary>
+        /// The callback function called when the panel change from Background state to Active state.
+        /// </summary>
+        void OnBackground2Active();
+
+        /// <summary>
+        /// The callback function called when the panel change from Active state to Background state.
+        /// </summary>
+        void OnActive2Background();
+
+        /// <summary>
+        /// The async callback function called when the panel change from Active state to Background state.
+        /// </summary>
+        void OnActive2BackgroundAsync();
+
+        /// <summary>
+        /// The callback function called when the panel change from Active state to Inactive state.
+        /// </summary>
+        void OnActive2Inactive();
+
+        /// <summary>
+        /// The async callback function called when the panel changed from Active state to Inactive state.
+        /// </summary>
+        void OnActive2InactiveAsync();
+
+        /// <summary>
+        /// The callback function called when the panel changed from the Background state to Inactive state.
+        /// </summary>
+        void OnBackground2Inactive();
+
+        /// <summary>
+        /// The async callback function called when the panel changed from Background state to Inactive state.
+        /// </summary>
+        void OnBackground2InactiveAsync();
+    }
+}
---
+++ Runtime/IUIPanelStack.cs
@@ -0,0 +1,17 @@
+﻿namespace FinTOKMAK.UIStackSystem.Runtime
+{
+    public interface IUIPanelStack
+    {
+        /// <summary>
+        /// The method to open a panel.
+        /// </summary>
+        /// <param name="panelElement">the panel to open</param>
+        void OpenPanel(IUIPanelElement panelElement);
+
+        /// <summary>
+        /// The method to close a panel.
+        /// </summary>
+        /// <param name="panelElement">the panel to close.</param>
+        void ClosePanel(IUIPanelElement panelElement);
+    }
+}
