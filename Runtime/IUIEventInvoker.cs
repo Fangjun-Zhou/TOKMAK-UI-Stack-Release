9f37c542e99fff156facce0fa11d752cae5ff6a1
Fangjun Zhou <fangjun.zhou02@gmail.com>
2021-12-30 18:35:13 -0600

    Added: IUIEventInvoker and UIEventInvoker for UI event handling

---
+++ Runtime/IUIEventInvoker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FinTOKMAK.UIStackSystem.Runtime
+{
+    /// <summary>
+    /// The universal interface for UI lifecycle event invoking.
+    /// </summary>
+    public interface IUIEventInvoker
+    {
+        /// <summary>
+        /// The action triggered when the panel(UI object) finish change from active to background.
+        /// </summary>
+        Action finishActive2Background { get; set; }
+        
+        /// <summary>
+        /// The action triggered when the panel(UI object) finish change from active to inactive.
+        /// </summary>
+        Action finishActive2Inactive { get; set; }
+        
+        /// <summary>
+        /// The action triggered when the panel(UI object) finish change from background to inactive.
+        /// </summary>
+        Action finishBackground2Inactive { get; set; }
+
+        /// <summary>
+        /// The method called to force trigger finishActive2Background event.
+        /// Always used by UI object animation event.
+        /// </summary>
+        void FinishActive2Background();
+
+        /// <summary>
+        /// The method called to force trigger finishActive2Inactive event.
+        /// Always used by UI object animation event.
+        /// </summary>
+        void FinishActive2Inactive();
+
+        /// <summary>
+        /// The method called to force trigger finishBackground2Inactive event.
+        /// Always used by UI object animation event.
+        /// </summary>
+        void FinishBackground2Inactive();
+    }
+}
---
+++ Runtime/UIEventInvoker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace FinTOKMAK.UIStackSystem.Runtime
+{
+    public class UIEventInvoker : MonoBehaviour, IUIEventInvoker
+    {
+        public Action finishActive2Background { get; set; }
+        public Action finishActive2Inactive { get; set; }
+        public Action finishBackground2Inactive { get; set; }
+        
+        public void FinishActive2Background()
+        {
+            finishActive2Background?.Invoke();
+        }
+
+        public void FinishActive2Inactive()
+        {
+            finishActive2Inactive?.Invoke();
+        }
+
+        public void FinishBackground2Inactive()
+        {
+            finishBackground2Inactive?.Invoke();
+        }
+    }
+}
