ac16b448d9e48a46b0acf3a42c82c73e85ac7056
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-01-19 14:39:33 -0600

    Update: UIStackManager. Implement stack initialization.

--- Runtime/UIStackManager.cs
+++ Runtime/UIStackManager.cs
@@ -1,12 +1,65 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using NaughtyAttributes;
 using UnityEngine;
 
 namespace FinTOKMAK.UIStackSystem.Runtime
 {
+    public class UIStackVirtualHead : IUIPanelElement
+    {
+        public PanelState state => PanelState.Inactive;
+
+        public void OnInitialization()
+        {
+            
+        }
+
+        public void OnInactive2Active()
+        {
+            
+        }
+
+        public void OnInactive2Background()
+        {
+            
+        }
+
+        public void OnBackground2Active()
+        {
+            
+        }
+
+        public void OnActive2Background()
+        {
+            
+        }
+
+        public async Task OnActive2BackgroundAsync()
+        {
+            return;
+        }
+
+        public void OnActive2Inactive()
+        {
+            
+        }
+
+        public async Task OnActive2InactiveAsync()
+        {
+            return;
+        }
+
+        public void OnBackground2Inactive()
+        {
+            
+        }
+
+        public async Task OnBackground2InactiveAsync()
+        {
+            return;
+        }
+    }
+    
     /// <summary>
     /// This is the Stack Manager for the UI Stack System
     /// This MonoBehaviour should be attached to every root GameObject of UIPanelElements
@@ -23,39 +76,79 @@ namespace FinTOKMAK.UIStackSystem.Runtime
         #region Private Field
 
         /// <summary>
-        /// The internal UI Stack
+        /// The head of the UI Stack.
         /// </summary>
-        private Stack<IUIPanelElement> _UIStack;
+        private UINode<IUIPanelElement> _stackHead;
 
         /// <summary>
-        /// A dummy head for internal UI Stack
+        /// The top of the stack.
         /// </summary>
-        private UIPanelElement _UIHead;
+        private UINode<IUIPanelElement> _top;
 
-        private UIPanelElement _top;
+        /// <summary>
+        /// The index of the stack top.
+        /// </summary>
+        private int _topIndex;
+
+        private Dictionary<IUIPanelElement, int> _uiPanelIndex = new Dictionary<IUIPanelElement, int>();
 
         #endregion
 
         private void Awake()
         {
-            /// Initialization for UI stack
-            _UIHead = new UIPanelElement();
-            _UIStack = new Stack<IUIPanelElement>();
-     
+            UINode<IUIPanelElement> virtualHead = new UINode<IUIPanelElement>()
+            {
+                value = new UIStackVirtualHead()
+            };
+            // Set the stack head and the stack top.
+            _stackHead = virtualHead;
+            _top = virtualHead;
+            _topIndex = 0;
+            _uiPanelIndex.Add(virtualHead.value, 0);
+
+            UINode<IUIPanelElement> curr = virtualHead;
+            // Push all the panels to the stack.
+            for (int i = UIPanels.Count-1; i >= 0; i--)
+            {
+                UINode<IUIPanelElement> node = new UINode<IUIPanelElement>()
+                {
+                    value = UIPanels[i]
+                };
+                curr.next = node;
+                node.prev = curr;
+
+                // Record the index
+                _uiPanelIndex.Add(UIPanels[i], UIPanels.Count - i);
+                
+                // Panel initialization
+                if (useInitializePanel && UIPanels[i] == initializationPanel)
+                {
+                    _top = node;
+                    _topIndex = UIPanels.Count - i;
+                }
+                
+                curr = node;
+            }
+            
+            // Set the top panel to active.
+            _top.value.OnInactive2Active();
         }
 
 
         private void Start()
         {
-            /// Initialization for UI stack and creates a dummy head
-            _UIStack.Push(_UIHead);
+            
         }
 
         #region UIStack Operation
 
         public void OpenPanel(IUIPanelElement panelElement)
         {
-            
+            // Check panel available
+            if (!_uiPanelIndex.ContainsKey(panelElement))
+            {
+                throw new InvalidOperationException("Panel not available.");
+            }
         }
 
         public async Task OpenPanelAsync(IUIPanelElement panelElement)
