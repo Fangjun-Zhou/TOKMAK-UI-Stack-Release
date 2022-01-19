110cdf67f75e4cae364cd5fb247c9ab7d04b80b2
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-01-19 15:22:57 -0600

    Update: UIStackManager Finish open panel and close panel implementation.

--- Runtime/UIStackManager.cs
+++ Runtime/UIStackManager.cs
@@ -7,7 +7,9 @@ namespace FinTOKMAK.UIStackSystem.Runtime
 {
     public class UIStackVirtualHead : IUIPanelElement
     {
-        public PanelState state => PanelState.Inactive;
+        private PanelState _state = PanelState.Background;
+        
+        public PanelState state => _state;
 
         public void OnInitialization()
         {
@@ -16,46 +18,49 @@ namespace FinTOKMAK.UIStackSystem.Runtime
 
         public void OnInactive2Active()
         {
-            
+            _state = PanelState.Active;
         }
 
         public void OnInactive2Background()
         {
-            
+            _state = PanelState.Background;
         }
 
         public void OnBackground2Active()
         {
-            
+            _state = PanelState.Active;
         }
 
         public void OnActive2Background()
         {
-            
+            _state = PanelState.Background;
         }
 
         public async Task OnActive2BackgroundAsync()
         {
+            _state = PanelState.Background;
             return;
         }
 
         public void OnActive2Inactive()
         {
-            
+            _state = PanelState.Inactive;
         }
 
         public async Task OnActive2InactiveAsync()
         {
+            _state = PanelState.Inactive;
             return;
         }
 
         public void OnBackground2Inactive()
         {
-            
+            _state = PanelState.Inactive;
         }
 
         public async Task OnBackground2InactiveAsync()
         {
+            _state = PanelState.Inactive;
             return;
         }
     }
@@ -90,7 +95,16 @@ namespace FinTOKMAK.UIStackSystem.Runtime
         /// </summary>
         private int _topIndex;
 
+        /// <summary>
+        /// The dictionary that maps panel to its index.
+        /// </summary>
         private Dictionary<IUIPanelElement, int> _uiPanelIndex = new Dictionary<IUIPanelElement, int>();
+
+        /// <summary>
+        /// The dictionary that maps panel to its node.
+        /// </summary>
+        private Dictionary<IUIPanelElement, UINode<IUIPanelElement>> _uiPanelNodes =
+            new Dictionary<IUIPanelElement, UINode<IUIPanelElement>>();
 
         #endregion
 
@@ -105,6 +119,7 @@ namespace FinTOKMAK.UIStackSystem.Runtime
             _top = virtualHead;
             _topIndex = 0;
             _uiPanelIndex.Add(virtualHead.value, 0);
+            _uiPanelNodes.Add(virtualHead.value, virtualHead);
 
             UINode<IUIPanelElement> curr = virtualHead;
             // Push all the panels to the stack.
@@ -119,6 +134,10 @@ namespace FinTOKMAK.UIStackSystem.Runtime
 
                 // Record the index
                 _uiPanelIndex.Add(UIPanels[i], UIPanels.Count - i);
+                _uiPanelNodes.Add(UIPanels[i], node);
+                
+                // Call the panel initialize.
+                UIPanels[i].OnInitialization();
                 
                 // Panel initialization
                 if (useInitializePanel && UIPanels[i] == initializationPanel)
@@ -134,12 +153,6 @@ namespace FinTOKMAK.UIStackSystem.Runtime
             _top.value.OnInactive2Active();
         }
 
-
-        private void Start()
-        {
-            
-        }
-
         #region UIStack Operation
 
         public void OpenPanel(IUIPanelElement panelElement)
@@ -149,21 +162,131 @@ namespace FinTOKMAK.UIStackSystem.Runtime
             {
                 throw new InvalidOperationException("Panel not available.");
             }
+            
+            // Check if the panel is already the top.
+            if (_top.value == panelElement)
+            {
+                Debug.LogWarning("Panel already open.");
+                return;
+            }
+
+            if (_uiPanelIndex[panelElement] > _topIndex)
+            {
+                // Close the top panel.
+                _top.value.OnActive2Background();
+                // Open the new panel.
+                panelElement.OnInactive2Active();
+                // Update the stack top pointer and index.
+                _top = _uiPanelNodes[panelElement];
+                _topIndex = _uiPanelIndex[panelElement];
+            }
+            else
+            {
+                // Switch panel to background.
+                panelElement.OnInactive2Background();
+            }
         }
 
         public async Task OpenPanelAsync(IUIPanelElement panelElement)
         {
-            throw new NotImplementedException();
+            // Check panel available
+            if (!_uiPanelIndex.ContainsKey(panelElement))
+            {
+                throw new InvalidOperationException("Panel not available.");
+            }
+            
+            // Check if the panel is already the top.
+            if (_top.value == panelElement)
+            {
+                Debug.LogWarning("Panel already open.");
+                return;
+            }
+
+            if (_uiPanelIndex[panelElement] > _topIndex)
+            {
+                // Close the top panel.
+                await _top.value.OnActive2BackgroundAsync();
+                // Open the new panel.
+                panelElement.OnInactive2Active();
+                // Update the stack top pointer and index.
+                _top = _uiPanelNodes[panelElement];
+                _topIndex = _uiPanelIndex[panelElement];
+            }
+            else
+            {
+                // Switch panel to background.
+                panelElement.OnInactive2Background();
+            }
         }
 
         public void ClosePanel(IUIPanelElement panelElement)
         {
-            throw new NotImplementedException();
+            // Check panel available
+            if (!_uiPanelIndex.ContainsKey(panelElement))
+            {
+                throw new InvalidOperationException("Panel not available.");
+            }
+            
+            // Check if the panel is already closed.
+            if (panelElement.state == PanelState.Inactive)
+            {
+                Debug.Log("Panel already closed.");
+                return;
+            }
+            
+            // If the target panel is the stack top.
+            // Check if the panel is already the top.
+            if (_top.value == panelElement)
+            {
+                _top.value.OnActive2Inactive();
+                // Try to find the new highest
+                while (_top.prev != null)
+                {
+                    _top = _top.prev;
+                    if (_top.value.state == PanelState.Background)
+                        break;
+                }
+                _top.value.OnBackground2Active();
+            }
+            else
+            {
+                panelElement.OnBackground2Inactive();
+            }
         }
 
         public async Task ClosePanelAsync(IUIPanelElement panelElement)
         {
-            throw new NotImplementedException();
+            // Check panel available
+            if (!_uiPanelIndex.ContainsKey(panelElement))
+            {
+                throw new InvalidOperationException("Panel not available.");
+            }
+            
+            // Check if the panel is already closed.
+            if (panelElement.state == PanelState.Inactive)
+            {
+                Debug.Log("Panel already closed.");
+                return;
+            }
+            
+            // If the target panel is the stack top.
+            // Check if the panel is already the top.
+            if (_top.value == panelElement)
+            {
+                await _top.value.OnActive2InactiveAsync();
+                // Try to find the new highest
+                while (_top.prev != null)
+                {
+                    _top = _top.prev;
+                    if (_top.value.state == PanelState.Background)
+                        break;
+                }
+                _top.value.OnBackground2Active();
+            }
+            else
+            {
+                await panelElement.OnBackground2InactiveAsync();
+            }
         }
 
         #endregion
