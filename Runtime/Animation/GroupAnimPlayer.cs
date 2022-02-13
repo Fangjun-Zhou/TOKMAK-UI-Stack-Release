be7993b222509be80081a2127b6252f9d5192c12
Fangjun Zhou <fangjun.zhou02@gmail.com>
2022-02-13 16:30:09 -0600

    Added: GroupAnimPlayer

---
+++ Runtime/Animation/GroupAnimPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace FinTOKMAK.UIStackSystem.Runtime
+{
+    /// <summary>
+    /// The player to play a group of animation.
+    /// </summary>
+    public class GroupAnimPlayer : MonoBehaviour
+    {
+        
+    }
+}
